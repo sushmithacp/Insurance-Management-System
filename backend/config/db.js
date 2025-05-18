519c839417d22185e5bd4f98f6807265eee346ed
sushmithacp <sushmithagowda186@gmail.com>
2025-05-18 01:20:11 -0700

    updated IMS

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 
  let connectDB = async () => {
     try {
-         await mongoose.connect(process.env.MONGO_URI);
+         await mongoose.connect(process.env.MONGO_URL);
         console.log("MongoDB Connected");
         
     } catch (error) {

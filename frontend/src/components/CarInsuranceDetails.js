53bee5e795e254691817390d5b3f861a41841643
sushmithacp <sushmithagowda186@gmail.com>
2025-05-20 22:54:49 -0700

    updated IMS

--- frontend/src/components/CarInsuranceDetails.js
+++ frontend/src/components/CarInsuranceDetails.js
@@ -1,7 +1,7 @@
 import { React,  useState, useEffect } from 'react'
 import Navbar from '../components/Navbar'
 import Footer from '../components/Footer'
-import jwtDecode from 'jwt-decode';
+import { jwtDecode } from 'jwt-decode'; 
 import axios from 'axios';
 
 function CarInsuranceDetails() {
